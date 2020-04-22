d8fa9e9d218a12036ac4a33eedaffc652191675d
kaylawoodbury <kaylasandoval92@gmail.com>
2020-04-22 15:07:01 +0200

    added user restrictions to comments spec test

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :comment do
-    body { 'Text here'}
+    body { 'Text here' }
     association :article, factory: :article
   end
 end
--- spec/requests/api/comments/create_spec.rb
+++ spec/requests/api/comments/create_spec.rb
@@ -1,27 +1,38 @@
-RSpec.describe 'POST /comments', type: :request do 
+# frozen_string_literal: true
 
-  describe 'successfully creates comment' do 
-    let!(:article) { create(:article) }
-    before do
-      post 'api/comments',
-      params: { 
-        comment: { 
-          body: 'comment text', 
-          article_id: article.id
-          }
-        }
-    end 
+RSpec.describe 'POST /comments', type: :request do
+  describe 'with valid credentials' do
+    let(:user) { create(:user) }
+    let(:user_credentials) { user.create_new_auth_token }
+    let(:user_headers) do
+      { HTTP_ACCEPT: 'application/json' }.merge!(user_credentials)
+    end
 
-    it 'returns 200 response' do 
-      expect(response.status).to eq 200
-    end 
+    describe 'successfully creates comment' do
+      let!(:article) { create(:article) }
+      before do
+        post 'api/comments',
+             params: {
+               comment: {
+                 body: 'comment text',
+                 article_id: article.id,
+                 user_id: user.id
+               }
+             },
+             headers: user_headers
+      end
 
-    it 'returns response message' do 
-      expect(response_json['message']).to eq 'Your comment has been added'
-    end 
+      it 'returns 200 response' do
+        expect(response.status).to eq 200
+      end
 
-    it 'comment belongs to article' do 
-      expect(Comment.article_id).to eq article.id
-    end 
-  end 
-end 
+      it 'returns response message' do
+        expect(response_json['message']).to eq 'Your comment has been added'
+      end
+
+      it 'comment belongs to article' do
+        expect(Comment.article_id).to eq article.id
+      end
+    end
+  end
+end
