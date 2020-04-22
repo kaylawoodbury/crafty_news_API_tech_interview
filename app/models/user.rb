f73f6f58e7938de842e42c97e0631cdce54f8f50
kaylawoodbury <kaylasandoval92@gmail.com>
2020-04-22 15:02:42 +0200

    created new user model with devise auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
-class User < ApplicationRecord
-  extend Devise::Models
-  devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable
+# frozen_string_literal: true
 
+class User < ActiveRecord::Base
+  extend Devise::Models
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable
+  include DeviseTokenAuth::Concerns::User
   has_many :comments
 end
